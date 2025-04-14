db59e13b07fa68e2f19cd5a1cd1fffc408c5f4b2
Fady Kuzman <fady.kuzman@pm.me>
2025-04-14 14:50:51 +0200

    Added creating budget from start to end
    - and refactored

--- src/main/java/dev/codefuchs/household_budget/application/budgets/BudgetsService.java
+++ src/main/java/dev/codefuchs/household_budget/application/budgets/BudgetsService.java
@@ -5,6 +5,7 @@ import dev.codefuchs.household_budget.adapters.out.persistence.budgets.BudgetDTO
 import dev.codefuchs.household_budget.adapters.out.persistence.budgets.BudgetsRepository;
 import dev.codefuchs.household_budget.application.budget_categories.BudgetCategoryAddedEvent;
 import dev.codefuchs.household_budget.domain.budget.Budget;
+import dev.codefuchs.household_budget.domain.budget_categories.BudgetCategory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -19,37 +20,34 @@ public class BudgetsService {
 
     @EventListener
     public void createBudgets(BudgetCategoryAddedEvent event) {
-        // for monthly budgets
-        // get all budgets for the category and time span
-        // create new budgets for each month
-        // save all
 
-        // if startdate doesn't exist - create only for current month
+        // TODO: should notify user if budgets already exist for any month of the period
+
         var budgetCategory = event.budgetCategory();
         if (event.startDate() == null) {
             YearMonth now = YearMonth.now();
-            var budget = Budget.builder()
-                    .amount(budgetCategory.getAmount())
-                    .year(now.getYear())
-                    .month(now.getMonthValue())
-                    .budgetCategory(budgetCategory)
-                    .build();
-            repository.save(budget);
-        }
-
-        // if startdate exists and enddate doesn't - create for all months from startdate to now
-        else if (event.endDate() == null) {
+            createBudget(budgetCategory, now);
+        } else if (event.endDate() == null) {
             YearMonth now = YearMonth.now();
-            // loop from startdate to now
-            for (YearMonth start = event.startDate(); start.isBefore(now.plusMonths(1)); start = start.plusMonths(1)) {
-                var budget = Budget.builder()
-                        .amount(budgetCategory.getAmount())
-                        .year(start.getYear())
-                        .month(start.getMonthValue())
-                        .budgetCategory(budgetCategory)
-                        .build();
-                repository.save(budget);
-            }
+            createBudgetsRetrospectively(event, now);
+        } else {
+            createBudgetsRetrospectively(event, event.endDate());
+        }
+    }
+
+    private void createBudget(BudgetCategory budgetCategory, YearMonth now) {
+        var budget = Budget.builder()
+                .amount(budgetCategory.getAmount())
+                .year(now.getYear())
+                .month(now.getMonthValue())
+                .budgetCategory(budgetCategory)
+                .build();
+        repository.save(budget);
+    }
+
+    private void createBudgetsRetrospectively(BudgetCategoryAddedEvent event, YearMonth now) {
+        for (YearMonth start = event.startDate(); start.isBefore(now.plusMonths(1)); start = start.plusMonths(1)) {
+            createBudget(event.budgetCategory(), start);
         }
     }
 
