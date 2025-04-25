f7e92dbf09839cdc41c1d2efe2624f1b7c3aecc7
Fady Kuzman <fady.kuzman@pm.me>
2025-04-25 23:40:48 +0200

    fix compose and migrations

--- src/main/resources/database/migrations/V1__initial_setup.sql
+++ src/main/resources/database/migrations/V1__initial_setup.sql
@@ -1,7 +1,6 @@
 CREATE TABLE budget_categories (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50),
-    amount INT,
     amount NUMERIC(19,2),
     currency VARCHAR(3) DEFAULT 'EUR',
     cadence VARCHAR(50),
@@ -13,7 +12,6 @@ CREATE TABLE budget_categories (
 
 CREATE TABLE budgets (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    amount INT,
     amount NUMERIC(19,2),
     currency VARCHAR(3) DEFAULT 'EUR',
     year INT,
@@ -26,7 +24,6 @@ CREATE TABLE expenses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     purpose VARCHAR(200),
     date DATE DEFAULT CURRENT_DATE,
-    amount INT,
     amount NUMERIC(19,2),
     currency VARCHAR(3) DEFAULT 'EUR',
     budget_id UUID,
@@ -37,7 +34,6 @@ CREATE TABLE expenses (
 CREATE TABLE compensations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     source VARCHAR(200),
-    amount INT,
     amount NUMERIC(19,2),
     currency VARCHAR(3) DEFAULT 'EUR',
     date DATE DEFAULT CURRENT_DATE,
