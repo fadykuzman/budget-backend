5b972550a529b68345a3c8b07ab7fa8bd4c3487f
Fady Kuzman <fady.kuzman@pm.me>
2025-04-25 21:12:25 +0200

    Consolidate migrations

--- src/main/resources/database/migrations/V1__initial_setup.sql
+++ src/main/resources/database/migrations/V1__initial_setup.sql
@@ -2,6 +2,8 @@ CREATE TABLE budget_categories (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50),
     amount INT,
+    amount NUMERIC(19,2),
+    currency VARCHAR(3) DEFAULT 'EUR',
     cadence VARCHAR(50),
     start_month INT,
     start_year INT,
@@ -12,8 +14,33 @@ CREATE TABLE budget_categories (
 CREATE TABLE budgets (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     amount INT,
+    amount NUMERIC(19,2),
+    currency VARCHAR(3) DEFAULT 'EUR',
     year INT,
     month INT,
     budget_category_id UUID,
     FOREIGN KEY (budget_category_id) REFERENCES budget_categories(id)
+);
+
+CREATE TABLE expenses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    purpose VARCHAR(200),
+    date DATE DEFAULT CURRENT_DATE,
+    amount INT,
+    amount NUMERIC(19,2),
+    currency VARCHAR(3) DEFAULT 'EUR',
+    budget_id UUID,
+    FOREIGN KEY (budget_id) REFERENCES budgets(id)
+);
+
+
+CREATE TABLE compensations (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    source VARCHAR(200),
+    amount INT,
+    amount NUMERIC(19,2),
+    currency VARCHAR(3) DEFAULT 'EUR',
+    date DATE DEFAULT CURRENT_DATE,
+    budget_id UUID,
+    FOREIGN KEY (budget_id) REFERENCES budgets(id)
 );
--- src/main/resources/database/migrations/V2__create_table_expenses.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE expenses (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    purpose VARCHAR(200),
-    amount INT,
-    budget_id UUID,
-    FOREIGN KEY (budget_id) REFERENCES budgets(id)
-);
