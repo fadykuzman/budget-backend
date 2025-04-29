e7396dbbc12b7e66c4e27f6e463594c91b307b62
Fady Kuzman <fady.kuzman@pm.me>
2025-04-29 12:36:48 +0200

    allow specific hosts

--- src/main/java/dev/codefuchs/household_budget/config/WebConfig.java
+++ src/main/java/dev/codefuchs/household_budget/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://192.168.178.69:3100", "http://localhost:3100", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
