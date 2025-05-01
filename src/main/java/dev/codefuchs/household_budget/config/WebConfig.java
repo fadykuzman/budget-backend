5f007f1a77c6d9d5f7b2e8249de8c313f868a002
Fady Kuzman <fady.kuzman@pm.me>
2025-05-01 15:17:49 +0200

    Make cors allowedOrigins configurable according to env

--- src/main/java/dev/codefuchs/household_budget/config/WebConfig.java
+++ src/main/java/dev/codefuchs/household_budget/config/WebConfig.java
@@ -1,19 +1,24 @@
 package dev.codefuchs.household_budget.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig {
+    private final CorsProperties corsProperties;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.178.69:3100", "http://localhost:3100", "http://localhost:3000")
+                        .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
                         .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
